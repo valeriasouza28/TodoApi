28b88a7dc125557a4abb29c0d6af301c120fa2fd
valeriasouza28 <82419677+valeriasouza28@users.noreply.github.com>
2024-04-25 13:15:17 +0000

    adicionado javascript

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,8 +1,11 @@
-namespace TodoApi.Models;
 
-public class TodoItem
+namespace TodoApi.Models
 {
-    public long Id { get; set; }
-    public string? Name { get; set; }
-    public bool IsComplete { get; set; }
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string? Secret { get; set; }
+    }
 }
